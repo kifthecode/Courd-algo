9beb63d3ea2fc4733fce472579a2a9ac3b063a13
kiffthecode <laydi.soilihi@2isa.org>
2026-09-24 11:50:51 +0200

    exo a refaire

--- Exercice30/Program.cs
+++ Exercice30/Program.cs
@@ -14,11 +14,11 @@ for (int i = 0; i < entiers.Length; i++)
     Console.WriteLine("Veuillez entrer un nombre entier ({0}/8) :", i + 1);
     string? saisie = Console.ReadLine();
     // Correction CS8604 : Vérification de la nullité de la saisie
-    //while (string.IsNullOrWhiteSpace(saisie) || !int.TryParse(saisie, out entiers[i]))
-    //{
-    //    Console.WriteLine("Entrée invalide. Veuillez entrer un nombre entier valide :");
-    //  saisie = Console.ReadLine();
-    //}
+    while (string.IsNullOrWhiteSpace(saisie) || !int.TryParse(saisie, out entiers[i]))
+    {
+        Console.WriteLine("Entrée invalide. Veuillez entrer un nombre entier valide :");
+        saisie = Console.ReadLine();
+    }
 }
 
 
@@ -29,11 +29,13 @@ for (int i = 0; i < chaines.Length; i++)
     Console.WriteLine("Veuillez entrer 10 prenoms ({0}/10) :", i + 1);
     string? saisie = Console.ReadLine();
     // Correction de la logique de saisie pour les prénoms (pas de int.TryParse ici)
-    //while (string.IsNullOrWhiteSpace(saisie))
-    //{
-    //    Console.WriteLine("Entrée invalide. Veuillez entrer un vrai prenom valide :");
-    //    saisie = Console.ReadLine();
-    //}
+    while (string.IsNullOrWhiteSpace(saisie))
+    {
+        Console.WriteLine("Entrée invalide. Veuillez entrer un vrai prenom valide :");
+        saisie = Console.ReadLine();
+    }
+
+
     chaines[i] = saisie;
 }
 
@@ -42,12 +44,12 @@ for (int i = 0; i < prix.Length; i++)
 {
     Console.WriteLine("Veuillez entrer un prix ({0}/5) :", i + 1);
     string? saisie = Console.ReadLine();
-    // Correction : Vérification de la nullité et conversion en double
-    //while (string.IsNullOrWhiteSpace(saisie) || !double.TryParse(saisie, out prix[i]))
-    //{
-    //    Console.WriteLine("Entrée invalide. Veuillez entrer un prix valide :");
-    //    saisie = Console.ReadLine();
-    //}
+    //Correction: Vérification de la nullité et conversion en double
+    while (string.IsNullOrWhiteSpace(saisie) || !double.TryParse(saisie, out prix[i]))
+    {
+        Console.WriteLine("Entrée invalide. Veuillez entrer un prix valide :");
+        saisie = Console.ReadLine();
+    }
 }
 
 
