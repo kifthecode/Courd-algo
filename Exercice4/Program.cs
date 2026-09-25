1321886a93eb7dae82e339ada343ec589efefa7f
kiffthecode <laydi.soilihi@2isa.org>
2026-09-25 08:45:18 +0200

    exo a refaire

--- Exercice4/Program.cs
+++ Exercice4/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// la cuisine prépare des bacs de pate : 500 g
+//// pour le premier,250 g de plus a chaque bac
+//Console.Write("Combien de bacs ?");
+
+//int nbBacs;
+
+//int.TryParse(Console.ReadLine(), out nbBacs);
+
+//// La taille vient de la saisie : new crée
+//// nbBacs cases, numerotéés de 0 a nbBacs - 1.
+//int[] bacs = new int[nbBacs];
+
+
+//// Premier parcours  : remplir.
+//for (int i = 0; i < nbBacs - 1; i++)
+//{
+//    bacs[i] = 500 + 250 * i;
+//}
+
+//// Second parcourss : afficher
+//for (int i = 0; i < nbBacs - 1; i++)
+//{
+//    Console.WriteLine($"bac {i + 1} : {bacs[i]} g");
+//}
+
+int[] t = new int[7];
+
+for (int i = 0; i < t.Length; i++)
+{
+    t[i] = 0;
+}
+
+
+foreach (var item in t)
+{
+    Console.WriteLine(item);
+}
--- Exercice5/Program.cs
+++ Exercice5/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Un tableau de 8 entiers nommé entiers
+int[] entiers = new int[8];
+
+// un tableau de 10 chaînes de caractères nommé chaines
+string[] chaines = new string[10];
+
+// un tableau de 5 doubles nommé prix
+double[] prix = new double[5];
+
+// Remplir entiers
+for (int i = 0; i < entiers.Length; i++)
+{
+    Console.Write($"Entrez l'entier {i + 1} : ");
+    entiers[i] = int.Parse(Console.ReadLine());
+}
+
+// Remplir chaines
+for (int i = 0; i < chaines.Length; i++)
+{
+    Console.Write($"Entrez la chaîne {i + 1} : ");
+    chaines[i] = Console.ReadLine();
+}
+
+// Remplir prix
+for (int i = 0; i < prix.Length; i++)
+{
+    Console.Write($"Entrez le prix {i + 1} : ");
+    prix[i] = double.Parse(Console.ReadLine());
+}
+
+// Appeler la fonction pour afficher les tableaux
+AfficherTableaux(entiers, chaines, prix);
+
+
+
+
+
+void AfficherTableaux(int[] tab1, string[] tab2, double[] tab3)
+{
+    Console.WriteLine("\nAffichage des tableaux :");
+    Console.WriteLine("\nTableau d'entiers :");
+    for (int i = 0; i < tab1.Length; i++)
+    {
+        Console.WriteLine(tab1[i]);
+    }
+
+    Console.WriteLine("\nTableau de chaînes :");
+    for (int i = 0; i < tab2.Length; i++)
+    {
+        Console.WriteLine(tab2[i]);
+    }
+
+    Console.WriteLine("\nTableau de prix :");
+    for (int i = 0; i < tab3.Length; i++)
+    {
+        Console.WriteLine(tab3[i]);
+    }
+}
